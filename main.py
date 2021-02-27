4916398490914a702a11c600c3df2ab383b26249
lumafepe <lumafepe@gmail.com>
2021-02-27 11:32:22 +0000

    erro de datas

--- main.py
+++ main.py
@@ -4,20 +4,14 @@ import time
 import os
 import logging
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler 
+from watchdog.events import FileSystemEventHandler
 import loader
 import datetime
 import horario
 import ocr
 
-#testes para ja!
-a=open("closeapp.dat",'w')
-a.write("0")
-a.close()
-a=open("off.dat",'w')
-a.write("1")
-a.close()
-os.system("python3 switch.py")
+
+
 #user
 user = getpass.getuser()
 
@@ -26,7 +20,7 @@ pics = f"/home/{user}/Pictures/"
 
 pics = f"/home/{user}/Pictures/"
 #diretorio para guardar as aulas
-pathpasta = "/home/{}/Documents/aulas/".format(user)
+pathpasta = f"/home/{user}/Documents/aulas/"
 
 #cria a pasta da aulas se nao existir
 os.system("mkdir -p " + pathpasta)
@@ -37,46 +31,46 @@ hor = (a.read()).split('\n') # horarios
 a.close()
 
 #event logger
-if __name__ == "__main__": 
-    my_event_handler = PatternMatchingEventHandler("*","", True, True)
-    #novo ficheiro criado
-    def on_created(event):
-
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
         #horas a que foi criado
         now = datetime.datetime.now()
+                
+        a,b,c=str( now.day),str( now.month),str( now.year)
         #que aula o alno esta a ter
         cadeira = horario.faztudo(datetime.datetime.today().weekday(),now.hour,now.minute,hor)
+                
+        base = f"{pathpasta}{cadeira}/{a}-{b}-{b}"
+        print(base)
         #cria pastas para essa aula
-        a,b,c=str( now.day),str( now.month),str( now.year)
-        os.system("mkdir -p "+ pathpasta + cadeira + '/' + a+'-'+b+'-'+c )
+        os.system(f"mkdir -p {base}" )
+                
         #nome da nova foto
         ficheirocriado = loader.up(pics)
-        # mover para o novo sitio
-        nome = len(os.listdir(pathpasta + cadeira + '/' + a+'-'+b+'-'+c))
+        nome = len(os.listdir(base))
         if nome>1:
             nome=str(nome)
         else:
             nome = str(nome+1)
-        os.system("mv " + pics + f'"{ficheirocriado}"' +" " + pathpasta + cadeira + '/' + a+'-'+b+'-'+c + '/' + nome + '.png')
-        #adicionado ficheiro de texto
-        os.system("touch " + pathpasta + cadeira + '/' + a+'-'+b+'-'+c + "/texto.txt")
-        #obtem texto
+        # mover para o novo sitio
+        os.system("mv " + pics + f'"{ficheirocriado}"' +" " + base)
+        # adicionado ficheiro de texto
+        os.system(f"touch {base}/texto.txt")
+
+        # obtem texto
         time.sleep(1)
-        if ficheirocriado=="ocr.png":
-            ocr.inseretexto(pathpasta + cadeira + '/' + a+'-'+b+'-'+c + "/" + nome + '.png')
+                
+        ocr.inseretexto(base,ficheirocriado)
+    
+if __name__ == "__main__":
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=pics, recursive=False)
+    observer.start()
 
-
-
-    my_event_handler.on_created = on_created
-
-    go_recursively = True
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, pics, recursive=go_recursively)
-
-    my_observer.start()
     try:
         while True:
-            time.sleep(5)
-    except:
-        my_observer.stop()
-    my_observer.join()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
