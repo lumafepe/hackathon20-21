c8704e3f53a3e0c56fa6af0d534cf62f22207dee
lumafepe <lumafepe@gmail.com>
2021-02-27 11:25:07 +0000

    adicionada class handler

--- main2.py
+++ main2.py
@@ -4,22 +4,14 @@ import time
 import os
 import logging
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler 
+from watchdog.events import FileSystemEventHandler
 import loader
 import datetime
 import horario
 import ocr
 
-"""
-#testes para ja!
-a=open("closeapp.dat",'w')
-a.write("0")
-a.close()
-a=open("off.dat",'w')
-a.write("1")
-a.close()
-os.system("python3 switch.py")
-"""
+
+
 #user
 user = getpass.getuser()
 
@@ -39,55 +31,46 @@ hor = (a.read()).split('\n') # horarios
 a.close()
 
 #event logger
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        #horas a que foi criado
+        now = datetime.datetime.now()
+                
+        a,b,c=str( now.day),str( now.month),str( now.year)
+        #que aula o alno esta a ter
+        cadeira = horario.faztudo(datetime.datetime.today().weekday(),now.hour,now.minute,hor)
+                
+        base = f"{pathpasta}{cadeira}/{a}-{b}-{b}"
+        print(base)
+        #cria pastas para essa aula
+        os.system(f"mkdir -p {base}" )
+                
+        #nome da nova foto
+        ficheirocriado = loader.up(pics)
+        nome = len(os.listdir(base))
+        if nome>1:
+            nome=str(nome)
+        else:
+            nome = str(nome+1)
+        # mover para o novo sitio
+        os.system("mv " + pics + f'"{ficheirocriado}"' +" " + base)
+        # adicionado ficheiro de texto
+        os.system(f"touch {base}/texto.txt")
+
+        # obtem texto
+        time.sleep(1)
+                
+        ocr.inseretexto(base,ficheirocriado)
     
-my_event_handler = PatternMatchingEventHandler("*","", True, True)
-#novo ficheiro criado
-def on_created(event):
+if __name__ == "__main__":
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=pics, recursive=False)
+    observer.start()
 
-    #horas a que foi criado
-    now = datetime.datetime.now()
-    
-    a,b,c=str( now.day),str( now.month),str( now.year)
-    #que aula o alno esta a ter
-    cadeira = horario.faztudo(datetime.datetime.today().weekday(),now.hour,now.minute,hor)
-    
-    base = f"{pathpasta}{cadeira}/{a}-{b}-{b}"
-    print(base)
-    #cria pastas para essa aula
-    os.system(f"mkdir -p {base}" )
-    
-    #nome da nova foto
-    ficheirocriado = loader.up(pics)
-    nome = len(os.listdir(base))
-    if nome>1:
-        nome=str(nome)
-    else:
-        nome = str(nome+1)
-    # mover para o novo sitio
-    os.system("mv " + pics + f'"{ficheirocriado}"' +" " + base)
-    # adicionado ficheiro de texto
-    os.system(f"touch {base}/texto.txt")
-
-    # obtem texto
-    time.sleep(1)
-    
-   
-    ocr.inseretexto(base,ficheirocriado)
-
-
-
-
-my_event_handler.on_created = on_created
-
-go_recursively = True
-my_observer = Observer()
-my_observer.schedule(my_event_handler, pics, recursive=go_recursively)
-
-my_observer.start()
-
-try:
-    while True:
-        time.sleep(5)
-except:
-    my_observer.stop()
-my_observer.join()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
