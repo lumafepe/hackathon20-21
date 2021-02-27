0840c21e811cac384849cbfa8eee60c7fca2c7dc
lumafepe <lumafepe@gmail.com>
2021-02-27 14:52:53 +0000

    adicionado possibilidade de os nomes conterem .

--- main2.py
+++ main2.py
@@ -33,10 +33,6 @@ pathpasta+=data["nome da pasta onde guardar"]
 #cria a pasta da aulas se nao existir
 os.system("mkdir -p " + pathpasta)
 
-#horario em string
-#a = open('Horarios/horario.txt', 'r')
-#hor = (a.read()).split('\n') # horarios
-#a.close()
 
 #event logger
 class MyHandler(FileSystemEventHandler):
@@ -44,43 +40,48 @@ class MyHandler(FileSystemEventHandler):
         #horas a que foi criado
         now = datetime.datetime.now()
         datadodia = ""
+        #dia-mes-ano
         dia,mes,ano=str( now.day),str( now.month),str( now.year)
-        if data["formato de datas"]!="":
-            ordem=(data["formato de datas"]).split('-')
-            for i in ordem:
-                if "ano"==i:
-                    datadodia+=ano+"-"
-                elif "mes"==i:
-                    datadodia+=mes+"-"
-                else:
-                    datadodia+=dia+"-"
+        ordem=(data["formato de datas"]).split('-')
+        for i in ordem:
+            if "ano"==i:
+                datadodia+=ano+"-"
+            elif "mes"==i:
+                datadodia+=mes+"-"
+            else:
+                datadodia+=dia+"-"
         datadodia=datadodia[:-1]
 
         #que aula o alno esta a ter
         #cadeira = horario.faztudo(datetime.datetime.today().weekday(),now.hour,now.minute,hor)
         cadeira = horario.disciplinasdodia(datetime.datetime.today().weekday(),now.hour,now.minute)
-
-        base = f"{pathpasta}{cadeira}/{datadodia}"
-        print(base)
-        #cria pastas para essa aula
-        os.system(f"mkdir -p {base}" )
+        if cadeira!="NADA":
+            base = f"{pathpasta}{cadeira}/{datadodia}"
+            print(base)
+            #cria pastas para essa aula
+            os.system(f"mkdir -p {base}" )
                 
-        #nome da nova foto
-        ficheirocriado = loader.up(pics)
-        fc=ficheirocriado
-        n=1
-        while os.path.exists(base + '/' + fc):
-            nome,tipo = fc.split('.')
-            fc = nome+f"-v{n}."+tipo
-            n+=1
-        # mover para o novo sitio
-        os.system("mv " + pics + f'"{ficheirocriado}"' +" " + base +'/'+fc)
-        # adicionado ficheiro de texto
+            #nome da nova foto
+            ficheirocriado = loader.up(pics)
+            fc=ficheirocriado
+            n=1
+            while os.path.exists(base + '/' + fc):
+                nome = fc.split('.')
+                tipo=nome.pop()
+                nome=""
+                for i in nome:
+                    nomes+=i+","
+                nomes=nomes[:-1]
+                fc = nomes+f"-{n}."+tipo
+                n+=1
+            # mover para o novo sitio
+            os.system("mv " + pics + f'"{ficheirocriado}"' +" " + base +'/'+fc)
+            # adicionado ficheiro de texto
 
-        # obtem texto
-        time.sleep(1)
-                
-        ocr.inseretexto(base,ficheirocriado)
+            # obtem texto
+            time.sleep(1)
+                    
+            ocr.inseretexto(base,ficheirocriado)
     
 if __name__ == "__main__":
     event_handler = MyHandler()
